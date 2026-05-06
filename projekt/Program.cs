0d0321e4da384833e5417c0a013d7be4070e57c9
Michal <267728@vut.cz>
2026-05-06 17:14:08 +0200

    connect openmeteo provider

--- projekt/Program.cs
+++ projekt/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace projekt;
+﻿using projekt.Providers;
+
+namespace projekt;
 
 class Program
 {
-    static void Main()
+    static async Task Main()
     {
         Console.WriteLine("Weather App");
+
+        var provider = new OpenWeatherProvider();
+
+        var weather = await provider.GetWeatherAsync("Prague");
+
+        Console.WriteLine($"City: {weather.City}");
+        Console.WriteLine($"Temperature: {weather.Temperature} °C");
+        Console.WriteLine($"Humidity: {weather.Humidity}%");
+        Console.WriteLine($"Wind: {weather.WindSpeed} km/h");
     }
 }
